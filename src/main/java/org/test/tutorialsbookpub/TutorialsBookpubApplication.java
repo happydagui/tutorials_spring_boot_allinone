821653652ba9f7c6cb13bd8c818cac004d359e79
happydagui <happydagui@126.com>
2017-11-11 17:02:34 +0800

    intial commit

---
+++ src/main/java/org/test/tutorialsbookpub/TutorialsBookpubApplication.java
@@ -0,0 +1,12 @@
+package org.test.tutorialsbookpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsBookpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialsBookpubApplication.class, args);
+	}
+}
